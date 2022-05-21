00d51a5343852ffd3517b09f32183237a240475c
Carl Myers <cmyers@cmyers.org>
2022-05-20 17:12:29 -0700

    Game implementation

--- src/main/java/org/cmyers/homework/tictactoe/Board.java
+++ src/main/java/org/cmyers/homework/tictactoe/Board.java
@@ -1,6 +1,8 @@
 package org.cmyers.homework.tictactoe;
 
+import java.nio.CharBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import org.cmyers.homework.tictactoe.exceptions.IllegalMoveException;
 import org.cmyers.homework.tictactoe.exceptions.WrongPlayerException;
 
@@ -15,7 +17,12 @@ public class Board {
      * constructor for the board, initializes an empty board, sets isXTurn to true.
      */
     public Board() {
-        // TODO: write your code here
+        this.board = new char[][] {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+        };
+        this.isXTurn = true;
     }
 
     /**
@@ -37,8 +44,7 @@ public class Board {
      * @return 'X', 'O', or ' ' for empty
      */
     public char getBoardPosition(int row, int col) throws ArrayIndexOutOfBoundsException {
-        // TODO: write your code here
-        return '\0';
+        return board[row][col];
     }
 
     public boolean isXTurn() {
@@ -49,7 +55,20 @@ public class Board {
      * Returns true if current player can't move (because game is won, or tied).
      */
     public boolean isGameOver() {
-        // TODO: write your code here
+
+        // char streams don't exist, they are really just streams of ints, le sigh.
+        if (Arrays.stream(board).flatMapToInt(x -> CharBuffer.wrap(x).chars()).allMatch(x -> x != (int) ' ')) {
+            // all spaces are taken
+            return true;
+        }
+        // if not all spaces are taken, we need to detect a win.
+        // first look for horizontal wins
+        char winner = getWinningPlayer();
+        if (winner == 'X' || winner == 'O') {
+            return true;
+        }
+
+        // no winner and spaces left, game not over
         return false;
     }
 
@@ -57,8 +76,45 @@ public class Board {
      * Returns the winning player, 'X', 'O', or ' ' if game is tied or unfinished.
      */
     public char getWinningPlayer() {
-        // TODO: write your code here
-        return '\0';
+        // we have to detect all the possible ways to win
+        // after you make a winning move, it is the other player's turn, so a
+        // valid win state can only happen for the player whose turn it is not.
+        // This means we only have to test for one type of winner
+        char winningPlayer;
+        if (isXTurn) {
+            winningPlayer = 'O';
+        } else {
+            winningPlayer = 'X';
+        }
+        // horizontal
+        for (int i = 0; i < 3; ++i) {
+            if (board[i][0] == winningPlayer
+                    && board[i][1] == winningPlayer
+                    && board[i][2] == winningPlayer) {
+                return winningPlayer;
+            }
+        }
+        // vertical
+        for (int i = 0; i < 3; ++i) {
+            if (board[0][i] == winningPlayer
+                && board[1][i] == winningPlayer
+                && board[2][i] == winningPlayer) {
+                return winningPlayer;
+            }
+        }
+        // diagonals
+        if (board[0][0] == winningPlayer
+            && board[1][1] == winningPlayer
+            && board[2][2] == winningPlayer) {
+            return winningPlayer;
+        }
+        if (board[0][2] == winningPlayer
+                && board[1][1] == winningPlayer
+                && board[2][0] == winningPlayer) {
+            return winningPlayer;
+        }
+
+        return ' ';
     }
 
     /**
@@ -71,7 +127,20 @@ public class Board {
      * @throws WrongPlayerException if it is not the player's turn
      */
     public void makeMove(char player, int row, int col) throws IllegalMoveException, WrongPlayerException, ArrayIndexOutOfBoundsException {
-        // TODO: write your code here
+        if (isXTurn) {
+            if (player != 'X') {
+                throw new WrongPlayerException();
+            }
+        } else {
+            if (player != 'O') {
+                throw new WrongPlayerException();
+            }
+        }
+        if (board[row][col] != ' ') {
+            throw new IllegalMoveException();
+        }
+        board[row][col] = player;
+        isXTurn = !isXTurn;
     }
 
     /**
